609529f0a62991b76d3617e71901e112b7d5a2d6
Padma Dev Elango <padmadevsquil@gmail.com>
2026-05-02 22:43:15 +0530

    fix 0.1

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [],
   server: {
     // Allows all hosts so that any dynamic Cloudflare Tunnel URL works.
     allowedHosts: true, 
